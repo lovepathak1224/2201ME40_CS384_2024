bf915d4138a8bfbf7ca9d072ad2ec7585c08b634
lovepathak1224 <155883684+lovepathak1224@users.noreply.github.com>
2024-11-19 19:29:37 +0530

    Add files via upload

---
+++ tut06/tut06.py
@@ -0,0 +1,51 @@
+import re
+
+def validate_password(password, criteria):
+    if len(password) < 8:
+        print(f"'{password}' is Invalid. Less than 8 Characters.")
+        return False
+
+    uppercase = re.compile(r'[A-Z]')
+    lowercase = re.compile(r'[a-z]')
+    numbers = re.compile(r'[0-9]')
+    special_characters = re.compile(r'[!@#]')
+
+    criteria_checks = {
+        '1': uppercase.search(password),
+        '2': lowercase.search(password),
+        '3': numbers.search(password),
+        '4': special_characters.search(password)
+    }
+
+    if re.search(r'[^a-zA-Z0-9!@#]', password):
+        print(f"'{password}' is Invalid. It contains forbidden characters.")
+        return False
+
+    for i in criteria:
+        if not criteria_checks[i]:
+            print(f"'{password}' is Invalid. Missing criteria {i}.")
+            return False
+
+    print(f"'{password}' is Valid.")
+    return True
+
+# password_list
+password_list = [
+    "abc12345",
+    "abc",
+    "123456789",
+    "abcdefg$",
+    "abcdefgABHD!@313",
+    "abcdefgABHD$$!@313"
+]
+
+print("Select criteria for password validation:")
+print("1: Uppercase letters (A-Z)")
+print("2: Lowercase letters (a-z)")
+print("3: Numbers (0-9)")
+print("4: Special characters (!, @, #)")
+
+criteria = input("Enter the criteria that you want to check: ").split(',')
+
+for password in password_list:
+    validate_password(password,criteria)
